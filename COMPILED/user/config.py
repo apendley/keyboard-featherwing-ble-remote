13a7e8b638a249c1fdf65ac1642b5871f30416c4
Aaron Pendley <aaronpendley@gmail.com>
2025-03-18 15:11:02 -0600

    Missed the config file in the COMPILED/user directory

--- COMPILED/user/config.py
+++ COMPILED/user/config.py
@@ -1,17 +1,29 @@
 from collections import namedtuple
 
 # Calibration for touch screen
-TouchCalibration = namedtuple("TouchCalibration", ["min_x", "max_x", "min_y", "max_y", "invert_y"])
+TouchCalibration = namedtuple("TouchCalibration", [
+	"min_x", 
+	"max_x", 
+	"min_y", 
+	"max_y", 
+	"invert_y"]
+)
 
 # Device config settings
-DeviceConfig = namedtuple("DeviceConfig", ["ble_name", "neopixel_color", "neopixel_max_brightness", "low_voltage_cutoff", "touch_calibration"])
+DeviceConfig = namedtuple("DeviceConfig", [
+	"ble_name", 
+	"neopixel_color", 
+	"neopixel_max_brightness", 
+	"low_voltage_warning", 
+	"touch_calibration"]
+)
 
 # Feel free to edit these seetings as needed/desired.
 CONFIG = DeviceConfig(
 	ble_name="Keyboard Featherwing Remote",
 	neopixel_color=0x0000FF,
 	neopixel_max_brightness=0.04,
-	low_voltage_cutoff=3.6,
+	low_voltage_warning=3.6,
 
 	touch_calibration=TouchCalibration(
     	min_x=200,
